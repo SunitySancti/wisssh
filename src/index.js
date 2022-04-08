65a9771fccddd250ae697f74ae2255a7192076b4
SunSan <san.suns.son@yandex.ru>
2022-04-08 19:26:28 +0300

    like-pinterest layout is somehow works

--- src/index.js
+++ src/index.js
@@ -12,13 +12,11 @@ import './index.css'
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
