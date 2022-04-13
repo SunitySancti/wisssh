e4d3230c3e485f5265f12a8f09f1c76488838203
SunSan <san.suns.son@yandex.ru>
2022-04-13 19:14:40 +0300

    added logo & started make routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 
 import './App.scss';
-import { MyWishes } from 'pages/MyWishes'
+import { MyWishesPage } from 'pages/MyWishes'
+import { AppLayout } from 'containers/AppLayout';
+import { MultiColumnTapeLayout } from 'containers/MultiColumnTapeLayout';
 
 function App() {
   return (
     <Routes>
-        <Route path='/' element={<MyWishes />} />
+        <Route path='/' element={ <AppLayout/> }>
+          <Route index element={ <MultiColumnTapeLayout/> } />
+          <Route path='my-wishes/all' element={ <MultiColumnTapeLayout/> } />
+          <Route path='my-wishes/actual' element={ <MultiColumnTapeLayout/> } />
+          <Route path='my-wishes/done' element={ <MultiColumnTapeLayout/> } />
+        </Route>
     </Routes>
   );
 }
