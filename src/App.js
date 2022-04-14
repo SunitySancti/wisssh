03697e3dcaf7c8b049f06147df8b982f22edf8d9
SunSan <san.suns.son@yandex.ru>
2022-04-14 18:34:17 +0300

    added some routing in header and sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.scss';
-import { MyWishesPage } from 'pages/MyWishes'
-import { AppLayout } from 'containers/AppLayout';
+import { AppHeader } from 'containers/AppHeader';
+import { SideBar } from 'containers/SideBar';
 import { MultiColumnTapeLayout } from 'containers/MultiColumnTapeLayout';
 
 function App() {
   return (
     <Routes>
-        <Route path='/' element={ <AppLayout/> }>
-          <Route index element={ <MultiColumnTapeLayout/> } />
-          <Route path='my-wishes/all' element={ <MultiColumnTapeLayout/> } />
-          <Route path='my-wishes/actual' element={ <MultiColumnTapeLayout/> } />
-          <Route path='my-wishes/done' element={ <MultiColumnTapeLayout/> } />
+        <Route path='/' element={ <AppHeader/> } >
+          <Route index element={ <Navigate to='/my-wishes/actual' replace/> } />
+          <Route path='my-wishes/' element={ <SideBar/> } >
+            <Route index element={ <Navigate to='/my-wishes/actual' replace/> } />
+            <Route path='actual' element={ <MultiColumnTapeLayout/> } />
+            <Route path='done' element={ <MultiColumnTapeLayout/> } />
+            <Route path='all' element={ <MultiColumnTapeLayout/> } />
+          </Route>
+          <Route path='my-invites/' element={ <SideBar/> } >
+            <Route index element={ <Navigate to='/my-invites/actual' replace/> } />
+            <Route path='actual' element={ <MultiColumnTapeLayout/> } />
+            <Route path='past' element={ <MultiColumnTapeLayout/> } />
+            <Route path='presented-by-me' element={ <MultiColumnTapeLayout/> } />
+          </Route>
+          <Route path='wishlists/' element={ <SideBar/> } >
+            <Route index element={ <Navigate to='/my-wishes/actual' replace/> } />
+            <Route path='my/:id' element={ <MultiColumnTapeLayout/> } />
+            <Route path=':userId/:id' element={ <MultiColumnTapeLayout/> } />
+          </Route>
         </Route>
     </Routes>
   );
