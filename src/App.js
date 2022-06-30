1ca9973b03693e2d9ac81670e855937fa07ea0c2
SunSan <san.suns.son@yandex.ru>
2022-06-30 15:40:45 +0300

    refactored mct-layout, avoided jumping in wishCards depends on image loading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React, { useLayoutEffect } from 'react';
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import './App.scss';
 import { AppHeader } from 'containers/AppHeader'
 import { NavBar } from 'containers/NavBar'
-import { MultiColumnTapeLayout } from 'containers/MultiColumnTapeLayout'
-import { WishPage } from 'pages/WishPage'
-import { WishListPage } from 'pages/WishListPage'
+import { WishesPage } from 'pages/WishesPage'
+import { SingleWishPage } from 'pages/SingleWishPage'
+import { WishlistPage } from 'pages/WishlistPage'
 import { ListOfListsPage } from 'pages/ListOfListsPage'
-import { fetchWishes } from 'store/wishesSlice'
+import { fetchWishes, filterWishesByUser } from 'store/wishesSlice'
+import { filterWishlistsByUser } from 'store/wishlistSlice'
 
 const App = () => {
+  const currentUser = useSelector(state => state.users.current);
+  const wishes = useSelector(state => state.wishes);
   const dispatch = useDispatch();
   const loadWishes = () => dispatch(fetchWishes());
-  useLayoutEffect(() => {
-      loadWishes();
-  },[])
+  const filterWishes = () => dispatch(filterWishesByUser(currentUser));
+  const filterWishlists = () => dispatch(filterWishlistsByUser(currentUser));
+
+  useEffect(() => {
+    loadWishes();
+  },[currentUser]);
+
+  useEffect(() => {
+    filterWishes();
+    console.log('filtration of wishes finished successfully: ', wishes.ofCurrentUser)
+  },[wishes.all]);
+
+  useEffect(() => {
+    filterWishlists();
+  },[]);
 
   return (
     <Routes>
@@ -24,31 +39,31 @@ const App = () => {
           <Route index element={ <Navigate to='/my-wishes/items/actual' replace/> } />
           <Route path='my-wishes/items/' element={ <NavBar/> } >
             <Route index element={ <Navigate to='/my-wishes/items/actual' replace/> } />
-            <Route path='actual' element={ <MultiColumnTapeLayout/> } />
-            <Route path='completed' element={ <MultiColumnTapeLayout/> } />
-            <Route path='all' element={ <MultiColumnTapeLayout/> } />
-            <Route path='actual/:wishId' element={ <WishPage/> } />
-            <Route path='completed/:wishId' element={ <WishPage/> } />
-            <Route path='all/:wishId' element={ <WishPage/> } />
+            <Route path='actual' element={ <WishesPage/> } />
+            <Route path='completed' element={ <WishesPage/> } />
+            <Route path='all' element={ <WishesPage/> } />
+            <Route path='actual/:wishId' element={ <SingleWishPage/> } />
+            <Route path='completed/:wishId' element={ <SingleWishPage/> } />
+            <Route path='all/:wishId' element={ <SingleWishPage/> } />
           </Route>
           <Route path='my-wishes/lists/' element={ <NavBar/> } >
             <Route index element={ <ListOfListsPage/> } />
-            <Route path=':wishlistId/' element={ <WishListPage/> } />
-            <Route path=':wishlistId/:wishId' element={ <WishPage/> } />
+            <Route path=':wishlistId/' element={ <WishlistPage/> } />
+            <Route path=':wishlistId/:wishId' element={ <SingleWishPage/> } />
           </Route>
           <Route path='my-invites/items' element={ <NavBar/> } >
             <Route index element={ <Navigate to='/my-invites/items/reserved' replace/> } />
-            <Route path='reserved' element={ <MultiColumnTapeLayout/> } />
-            <Route path='presented' element={ <MultiColumnTapeLayout/> } />
-            <Route path='all' element={ <MultiColumnTapeLayout/> } />
-            <Route path='reserved/:wishId' element={ <WishPage/> } />
-            <Route path='presented/:wishId' element={ <WishPage/> } />
-            <Route path='all/:wishId' element={ <WishPage/> } />
+            <Route path='reserved' element={ <WishesPage/> } />
+            <Route path='completed' element={ <WishesPage/> } />
+            <Route path='all' element={ <WishesPage/> } />
+            <Route path='reserved/:wishId' element={ <SingleWishPage/> } />
+            <Route path='completed/:wishId' element={ <SingleWishPage/> } />
+            <Route path='all/:wishId' element={ <SingleWishPage/> } />
           </Route>
           <Route path='my-invites/lists/' element={ <NavBar/> } >
             <Route index element={ <ListOfListsPage/> } />
-            <Route path=':wishlistId/' element={ <WishListPage/> } />
-            <Route path=':wishlistId/:wishId' element={ <WishPage/> } />
+            <Route path=':wishlistId/' element={ <WishlistPage/> } />
+            <Route path=':wishlistId/:wishId' element={ <SingleWishPage/> } />
           </Route>
         </Route>
     </Routes>
